9e5c088872553ad2c6b1cbac388ead7d9d8f8617
Tyler Walker <walkert20@hanover.edu>
2019-11-23 21:17:10 -0500

    Added Menu screen
    Added a Menu screen to the mini project. Also added an automated test for jumping.

---
+++ GetABlockMoving/Start menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Node2D.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
--- GetABlockMoving/test/unit/test_example.gd
+++ GetABlockMoving/test/unit/test_example.gd
@@ -18,11 +18,11 @@ func after_all():
 #func test_assert_eq_number_not_equal():
 #	assert_eq(1, 2, "Should fail.  1 != 2")
 
-func test_assert_eq_number_equal():
-	assert_eq('asdf', 'asdf', "Should pass")
+#func test_assert_eq_number_equal():
+#	assert_eq('asdf', 'asdf', "Should pass")
 
-func test_assert_true_with_true():
-	assert_true(true, "Should pass, true is true")
+#func test_assert_true_with_true():
+#	assert_true(true, "Should pass, true is true")
 	
 func test_movement():
 	var right = Input.is_action_pressed("ui_right")
@@ -37,3 +37,9 @@ func test_movement2():
 	while left:
 		assert_eq(test_player.motion.x, min(test_player.motion.x-test_player.ACCELERATION, 
 		-test_player.MAX_SPEED))
+		
+func test_movement3():
+	var jump = Input.is_action_pressed("ui_up")
+	assert_eq(jump, Input.is_action_pressed("ui_up"))
+	while jump:
+		assert_eq(test_player.motion.y, test_player.JUMP_HEIGHT)
