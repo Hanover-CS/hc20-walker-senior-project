a54ea711800c5e6618a5cd67de5bc14c56080f05
Tyler Walker <walkert20@hanover.edu>
2020-01-22 21:43:30 -0500

    Minor edits and alterations to test files.

--- GetABlockMoving/test/unit/AutomatedTests.gd
+++ GetABlockMoving/test/unit/AutomatedTests.gd
@@ -1,4 +1,4 @@
-extends "res://addons/gut/test.gd";
+extends "res://addons/gut/test.gd"
 
 const testPlayer = preload("res://Player.gd")
 onready var test_player = testPlayer.new()
@@ -15,30 +15,21 @@ func before_all():
 func after_all():
 	gut.p("ran run teardown", 2)
 
-#func test_assert_eq_number_not_equal():
-#	assert_eq(1, 2, "Should fail.  1 != 2")
-
-#func test_assert_eq_number_equal():
-#	assert_eq('asdf', 'asdf', "Should pass")
-
-#func test_assert_true_with_true():
-#	assert_true(true, "Should pass, true is true")
-	
-func test_movement_right():
+func test_movement():
 	var right = Input.is_action_pressed("ui_right")
 	assert_eq(right, Input.is_action_pressed("ui_right"))
 	while right:
 		assert_eq(test_player.motion.x, min(test_player.motion.x+test_player.ACCELERATION, 
 		test_player.MAX_SPEED))
 		
-func test_movement_left():
+func test_movement2():
 	var left = Input.is_action_pressed("ui_left")
 	assert_eq(left, Input.is_action_pressed("ui_left"))
 	while left:
 		assert_eq(test_player.motion.x, min(test_player.motion.x-test_player.ACCELERATION, 
 		-test_player.MAX_SPEED))
 		
-func test_movement_jump():
+func test_movement3():
 	var jump = Input.is_action_pressed("ui_up")
 	assert_eq(jump, Input.is_action_pressed("ui_up"))
 	while jump:
---
+++ GetABlockMoving/test/unit/ManualTests.gd
@@ -0,0 +1,7 @@
+extends "res://addons/gut/test.gd"
+
+# This file contains the manual test for my project. These tests
+# will be based on simple expectations of gameplay, such as the 
+# player character moving to the right if the right arrow key is pressed.
+
+# MANUAL MOVEMENT TESTS:
