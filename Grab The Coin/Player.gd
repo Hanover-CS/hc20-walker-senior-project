59651fb585ad2334b16da13fd8e2f6b45b644f3f
Tyler Walker <walkert20@hanover.edu>
2020-01-24 23:23:37 -0500

    Copying of files.
    Copied files over from mini project to main project. Having current issues with World2 and WorldComplete files (transition error).

--- Grab The Coin/Player.gd
+++ Grab The Coin/Player.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
 var motion = Vector2()
+#contains x and y in a single variable
+
 const GRAVITY = 20
+#falling speed
 const ACCELERATION = 50
 const MAX_SPEED = 200
+#horizontal speed
 const JUMP_HEIGHT = -500
+#max jump height
 const UP = Vector2(0, -1)
 
 func _physics_process(delta):
@@ -13,15 +18,15 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
-		#$Sprite.flip_h = false
-		#$Sprite.play("run")
+		$Sprite.flip_h = false
+		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
-		#$Sprite.flip_h = true
-		#$Sprite.play("run")
+		$Sprite.flip_h = true
+		$Sprite.play("run")
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
-		#$Sprite.play("idle")
+		$Sprite.play("idle")
 		FRICTION = true
 		
 	if is_on_floor():
@@ -30,13 +35,16 @@ func _physics_process(delta):
 		if FRICTION == true:
 			motion.x = lerp(motion.x, 0, 0.2)
 			
-	#else:
-	#	if motion.y < 0:
-	#		$Sprite.play("jump")
-	#	else:
-	#		$Sprite.play("fall")
-	#	if FRICTION == true:
-	#		motion.x = lerp(motion.x, 0, 0.05)
+	else:
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+		if FRICTION == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 
 	motion = move_and_slide(motion, UP)
-
+	#pass
+	# perhaps plan on having an ai control the player for a demo
+	
+# When you want to add something to the resource file, go to Documents.
